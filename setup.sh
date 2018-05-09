33623f473c3cc91f99258b4373d0a20c2e87a0d0
Ian Seyler <ian.seyler@returninfinity.com>
2018-05-09 13:18:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mkdir os
 cd os
 git clone https://github.com/ReturnInfinity/Pure64.git
 git clone https://github.com/ReturnInfinity/BareMetal-kernel.git
-sed -i 's/call STAGE3/jmp 0x100000/g' Pure64/src/pure64.asm
+sed -i 's/call STAGE3/jmp 0x100000/g' Pure64/src/arch/x86_64/pure64.asm
 cd ..
 ./build.sh
